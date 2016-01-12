0c1bbfae411f5f7160c1ff839150f45bfae4aae3
Avery Sandborn <amcsandborn@gmail.com>
2016-01-12 14:34:49 -0500

    Update complete.R

--- Coursera/complete.R
+++ Coursera/complete.R
@@ -2,16 +2,15 @@
 # Programming Assignment 1: Part 2
 ##########
 
-##########
 # Author: Avery Sandborn
 # Date: January 11, 2016
 ##########
 
-##########
 # Write a function that reads a directory full of files and reports the number of 
 #   completely observed cases in each data file. The function should return a data 
 #   frame where the first column is the name of the file and the second column is 
 #   the number of complete cases. A prototype of this function follows
+
 ##########
 
 # Set working directory
