17562dd5911f18832887d4a6c922a87da748633d
Avery Sandborn <amcsandborn@gmail.com>
2016-02-10 14:56:34 -0500

    Update Sandborn_Assignment2.R

--- Spatial_Analysis_Techniques/Sandborn_Assignment2.R
+++ Spatial_Analysis_Techniques/Sandborn_Assignment2.R
@@ -48,3 +48,47 @@ plot(nztrees, add = TRUE)
 # Show range of variation in density seen at this bandwidth
 contour(dmap, add = TRUE)
 persp(dmap)
+
+########################
+
+library(spatstat)
+
+# Summarize anemone data
+data(anemones)
+summary(anemones)
+class(anemones) # already ppp object
+attach(anemones)
+
+# Plot points of anemones with bounding box
+plot(anemones)
+
+# Nearest Neighbor Test 
+# Examine clark and evans nearest neighbor index of aggregation R
+# Crude measure of cluserting or ordering of a point pattern 
+clarkevans(anemones)
+# R is ~1.39 >> uniform
+
+# visualization using KDE
+dmap <- density(anemones, 10, at = "pixels")
+plot(dmap)
+
+# Find optumal bandwidth for a given pattern
+bw <- bw.diggle(anemones)
+plot(bw)
+bw2 <- as.numeric(bw)
+bw2
+# bw2 is ~11.18 
+
+# KDE visualization with bet bandwidth and points added
+dmap <- density(anemones, 11.18, at = "pixels")
+plot(dmap)
+plot(anemones, add = TRUE)
+
+# Show range of variation in density seen at this bandwidth
+contour(dmap, add = TRUE)
+persp(dmap)
+
+# Create a historgram of the diameter of the anemones
+marks <- anemones$marks
+marks
+hist(marks, breaks = 1:8, col = "gray", labels = TRUE)
